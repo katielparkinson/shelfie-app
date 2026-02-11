b6e3c945cfcd26297b54993494c199b29304456d
Katie Parkinson <katieparkinson2406@gmail.com>
2026-02-10 22:17:09 -0700

    Create Light and Dark Themes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,21 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
 import React from "react";
 import { Slot, Stack } from "expo-router";
+import { Colors } from "../constants/Colors";
+import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+
   return (
-    <View style={{ flex: 1 }}>
+    <>
+      <StatusBar value="auto" />
       <Stack
         screenOptions={{
-          headerStyle: { backgroundColor: "#ddd" },
-          headerTintColor: "black",
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
         }}
       >
         <Stack.Screen name="index" options={{ title: "Home" }} />
         <Stack.Screen name="about" options={{ title: "About" }} />
         <Stack.Screen name="contact" options={{ title: "Contact" }} />
       </Stack>
-    </View>
+    </>
   );
 };
 
