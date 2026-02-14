c59b54f52198c0d95844b0e2f8d48f0e69d8128a
Katie Parkinson <katieparkinson2406@gmail.com>
2026-02-14 16:11:06 -0700

    Add route groups and nested components

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,4 @@
 import { StyleSheet, Text, useColorScheme, View } from "react-native";
-import React from "react";
 import { Slot, Stack } from "expo-router";
 import { Colors } from "../constants/Colors";
 import { StatusBar } from "expo-status-bar";
@@ -17,6 +16,7 @@ const RootLayout = () => {
           headerTintColor: theme.title,
         }}
       >
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ title: "Home" }} />
         <Stack.Screen name="about" options={{ title: "About" }} />
         <Stack.Screen name="contact" options={{ title: "Contact" }} />
